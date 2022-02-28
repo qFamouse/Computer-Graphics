efeb863ada461cd09fafe26ce301167a3e2cd652
qFamouse <famouse_ru@mail.ru>
2022-03-01 00:37:34 +0300

    Create ViewModel for Mosaic

---
+++ ComputerGraphics/ViewModel/MosaicViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ComputerGraphics.Models;
+
+namespace ComputerGraphics.ViewModel
+{
+    internal class MosaicViewModel : BaseViewModel
+    {
+        public string Width { get; set; }
+        public string Height { get; set; }
+
+        public MosaicViewModel()
+        {
+
+        }
+    }
+}
