85537bb7c8c558b92b54b6026d5e2be8e3721933
qFamouse <famouse_ru@mail.ru>
2022-03-01 18:21:46 +0300

    Complite Mosaic

--- ComputerGraphics/Settings/Mosaic.xaml.cs
+++ ComputerGraphics/Settings/Mosaic.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ColorPicker;
+using ComputerGraphics.ViewModel;
 
 namespace ComputerGraphics.Settings
 {
@@ -23,6 +26,12 @@ namespace ComputerGraphics.Settings
         public Mosaic()
         {
             InitializeComponent();
+        }
+
+        private void NumberValidation_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
--- ComputerGraphics/ViewModel/MosaicViewModel.cs
+++ ComputerGraphics/ViewModel/MosaicViewModel.cs
@@ -1,21 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using ColorPicker;
 using ComputerGraphics.Models;
 
 namespace ComputerGraphics.ViewModel
 {
     internal class MosaicViewModel : BaseViewModel
     {
-        public string Width { get; set; }
-        public string Height { get; set; }
+        public Canvas Canvas => MainCanvas.GetInstance().Canvas;
+
+        public double Width
+        {
+            get => Canvas.Width;
+            set => Canvas.Width = value;
+        }
+
+        public double Height
+        {
+            get => Canvas.Height;
+            set => Canvas.Height = value;
+        }
+
+        public int BlockSize { get; set; } = 2;
+
+        public ObservableCollection<int> BlockSizes { get; set; } = new ObservableCollection<int>() {2, 4, 8};
+
+
+        public List<Color> MosaicColors { get; set; }
 
         public MosaicViewModel()
         {
 
+            MosaicColors = new List<Color>()
+            {
+                Colors.DarkGreen,
+                Colors.DarkOrange,
+                Colors.DarkRed,
+                Colors.DarkKhaki
+            };
+        }
+
+        public RelayCommand DrawCommand
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    Canvas.Children.Clear();
+                    Random random = new Random();
+                    for (int i = 0; i < Height; i += BlockSize)
+                    {
+                        for (int j = 0; j < Width; j += BlockSize)
+                        {
+                            var rect = new Rectangle()
+                            {
+                                Width = BlockSize,
+                                Height = BlockSize,
+                                Fill = new SolidColorBrush(MosaicColors[random.Next(0, MosaicColors.Count)])
+                            };
+
+                            Canvas.SetTop(rect, i);
+                            Canvas.SetLeft(rect, j);
+
+                            Canvas.Children.Add(rect);
+                            
+                        }
+                    }
+                });
+            }
         }
     }
 }
