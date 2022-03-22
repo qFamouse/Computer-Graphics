e58448e913ae51c4411eaff23c97d36e28c4016f
Famouse <famouse_ru@mail.ru>
2022-03-22 22:17:37 +0300

    Adding LZW data compression algorithm

---
+++ ComputerGraphics/Utils/Coding/LZW/LzwCoding.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Utils.Coding.LZW
+{
+    internal sealed class LzwCoding
+    {
+        private LzwDictionary _lzwDictionary;
+
+        public LzwCoding(LzwDictionary lzwDictionary)
+        {
+            _lzwDictionary = lzwDictionary ?? throw new ArgumentNullException(nameof(lzwDictionary));
+        }
+        
+        public byte[] Encode(string text)
+        {
+            var currentSequence = String.Empty;
+            LinkedList<ushort> encodedCharacters = new LinkedList<ushort>();
+
+            foreach (char nextChar in text)
+            {
+                string bufferSequence = currentSequence + nextChar;
+
+                if (_lzwDictionary.ContainsKey(bufferSequence))
+                {
+                    currentSequence = bufferSequence;
+                }
+                else
+                {
+                    _lzwDictionary.Add(bufferSequence, _lzwDictionary.Count);
+
+                    if (_lzwDictionary.TryGetValue(currentSequence, out ushort currentSequenceCode))
+                    {
+                        encodedCharacters.AddLast(currentSequenceCode);
+                        currentSequence = nextChar.ToString();
+                    }
+                    else
+                    {
+                        throw new AggregateException($"Can't get value from dictionary (Key: {currentSequence})");
+                    }
+                }
+            }
+            // Add the last element 
+            if (_lzwDictionary.TryGetValue(currentSequence, out ushort lastElementCode))
+            {
+                encodedCharacters.AddLast(lastElementCode);
+            }
+
+            return encodedCharacters.SelectMany(e => BitConverter.GetBytes(e)).ToArray();
+        }
+
+        public string Decode(byte[] encodedCharacters)
+        {
+            // Get firt sybmol
+            ushort previousWordCode = BitConverter.ToUInt16(encodedCharacters, 0);
+            _lzwDictionary.TryGetKey(previousWordCode, out string previousWordStr);
+
+            var decodedCharacters = new StringBuilder(previousWordStr);
+
+            for (int i = 2; i < encodedCharacters.Length; i += 2)
+            {
+                ushort currentWordCode = BitConverter.ToUInt16(encodedCharacters, i);
+                _lzwDictionary.TryGetKey(currentWordCode, out string currentWordStr);
+
+                if (_lzwDictionary.ContainsKey(currentWordStr))
+                {
+                    decodedCharacters.Append(currentWordStr);
+
+                    string bufferSequence = previousWordStr + currentWordStr.Substring(0, 1);
+                    if (!_lzwDictionary.ContainsKey(bufferSequence))
+                    {
+                        _lzwDictionary.Add(bufferSequence, _lzwDictionary.Count);
+                    }
+                }
+
+                previousWordStr = currentWordStr;
+            }
+
+            return decodedCharacters.ToString();
+        }
+    }
+}
---
+++ ComputerGraphics/Utils/Coding/LZW/LzwDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Utils.Coding.LZW
+{
+    internal sealed class LzwDictionary
+    {
+        private Dictionary<string, ushort> _encodeDictionary = new Dictionary<string, ushort>();
+        private Dictionary<ushort, string> _decodeDictionary = new Dictionary<ushort, string>();
+
+        public ushort Count => (ushort)_encodeDictionary.Count;
+
+        private void LoadDictionary(IList<string> dictionary)
+        {
+            for (int value = 0; value < dictionary.Count; value++)
+            {
+                Add(dictionary[value], (ushort)value);
+            }
+        }
+
+        public LzwDictionary(IList<string> dictionary) => LoadDictionary(dictionary);
+
+        public LzwDictionary()
+        {
+            List<string> dictionary = new List<string>()
+            {
+                "#", "a", "b", "w"
+            };
+
+            LoadDictionary(dictionary);
+        }
+
+        public bool ContainsKey(string key) => _encodeDictionary.ContainsKey(key);
+        public bool ContainsValue(ushort value) => _encodeDictionary.ContainsValue(value);
+        public void Add(string key, ushort value)
+        {
+            _encodeDictionary.Add(key, value);
+            _decodeDictionary.Add(value, key);
+
+            /* .NET Standart VERSION */
+
+            //bool encode = _encodeDictionary.TryAdd(key, value);
+            //bool decode = _decodeDictionary.TryAdd(value, key);
+
+            //if (encode ^ decode) // 1 0 or 0 1
+            //{
+            //    throw new AggregateException($"The consistency is broken when adding a new element (encode: {encode}, decode: {decode})");
+            //}
+
+            //return encode == decode;
+        }
+        public bool TryGetValue(string key, out ushort value) => _encodeDictionary.TryGetValue(key, out value);
+        public bool TryGetKey(ushort value, out string key) => _decodeDictionary.TryGetValue(value, out key);
+    }
+}
