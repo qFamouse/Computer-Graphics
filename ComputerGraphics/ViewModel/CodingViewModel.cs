36ce6512a8bc62455539704e1d8784d5e5f253b9
Famouse <famouse_ru@mail.ru>
2022-03-23 19:56:06 +0300

    Complite logic with coding window

---
+++ ComputerGraphics/ViewModel/CodingViewModel.cs
@@ -0,0 +1,111 @@
+﻿using ComputerGraphics.Models;
+using ComputerGraphics.Utils.Coding;
+using ComputerGraphics.Utils.Coding.LZW;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ComputerGraphics.ViewModel
+{
+    internal class CodingViewModel : BaseViewModel
+    {
+        private string _inputData;
+        private string _outputData;
+
+        public string InputData
+        {
+            get => _inputData;
+            set
+            {
+                _inputData = value;
+                OnPropertyChanged(nameof(InputData));
+            }
+        }
+        public string OutputData
+        {
+            get => _outputData;
+            set
+            {
+                _outputData = value;
+                OnPropertyChanged(nameof(OutputData));
+            }
+        }
+
+        #region Codings
+
+        private Coding _currentCoding = new Coding(null);
+
+        private LzwCoding _lzwCoding = new LzwCoding(new LzwDictionary());
+
+        #endregion
+
+        public CodingViewModel() { }
+
+        public RelayCommand SwitchToLzw
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    _currentCoding.CodingAlgorithm = _lzwCoding;
+                });
+            }
+        }
+
+
+        public RelayCommand EncodeCommand
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    try
+                    {
+                        OutputData = String.Join(" ", _currentCoding.Encode(InputData));
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        ShowErrorMessageBox("Please, choose the algorithm!");
+                    }
+                    catch (Exception e)
+                    {
+                        ShowErrorMessageBox(e.Message);
+                    }
+
+                });
+            }
+        }
+
+        public RelayCommand DecodeCommand
+        {
+            get
+            {
+                return new RelayCommand(obj =>
+                {
+                    try
+                    {
+                        byte[] encodedCharacters = OutputData.Split(' ').Select(s => Byte.Parse(s)).ToArray();
+                        OutputData = _currentCoding.Decode(encodedCharacters);
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        ShowErrorMessageBox("Please, choose the algorithm!");
+                    }
+                    catch (Exception e)
+                    {
+                        ShowErrorMessageBox(e.Message);
+                    }
+                });
+            }
+        }
+
+        private void ShowErrorMessageBox(string error)
+        {
+            MessageBox_Show(null, error, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
--- ComputerGraphics/Windows/Coding.xaml.cs
+++ ComputerGraphics/Windows/Coding.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ComputerGraphics.Models;
+using ComputerGraphics.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,13 @@ namespace ComputerGraphics.Windows
         public Coding()
         {
             InitializeComponent();
+
+            (this.DataContext as CodingViewModel).MessageBoxRequest += new EventHandler<MessageBoxEventArgs>(Coding_MessageBoxRequest);
+        }
+
+        private void Coding_MessageBoxRequest(object sender, MessageBoxEventArgs e)
+        {
+            e.Show(this);
         }
     }
 }
