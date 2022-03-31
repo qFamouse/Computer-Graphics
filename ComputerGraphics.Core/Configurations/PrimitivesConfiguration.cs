4c80891bfd41d1f5c00a88baaa4a8296e730fc3d
Famouse <famouse_ru@mail.ru>
2022-03-31 23:15:49 +0300

    Adding configuration for Primitives

---
+++ ComputerGraphics.Core/Configurations/PrimitivesConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using ComputerGraphics.Core.Entities;
+using ComputerGraphics.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace ComputerGraphics.Core.Configurations
+{
+    public static class PrimitivesConfiguration
+    {
+        public static List<PrimitiveFigures> PrimitiveFigures { get; } = new List<PrimitiveFigures>()
+        {
+            new PrimitiveFigures()
+            {
+                Image = new Image().FromUri(new Uri(@"..\Resources\Images\Primitives\option8.png", UriKind.Relative)),
+                Name = "Option 8",
+                r1 = (φ) => (1 + Math.Cos(2 * φ) * Math.Cos(2 * φ)) / 5,
+                r2 = (φ) => (1 + Math.Sin(3 * φ) * Math.Sin(3 * φ)) / 2
+            }
+        };
+    }
+}
--- ComputerGraphics.Core/Entities/PrimitiveFigures.cs
+++ ComputerGraphics.Core/Entities/PrimitiveFigures.cs
@@ -4,13 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
-namespace Core.Entities
+namespace ComputerGraphics.Core.Entities
 {
-    public sealed class PrimitiveFunction
+    public sealed class PrimitiveFigures
     {
         public Image Image { get; set; }
         public string Name { get; set; }
-        public Func<double, double> Function { get; set; }
+        public Func<double, double> r1 { get; set; }
+        public Func<double, double> r2 { get; set; }
     }
 }
