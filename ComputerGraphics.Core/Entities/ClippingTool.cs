3c292089d09968e3f4be9cdbf989d1fc82d6b4bc
Famouse <famouse_ru@mail.ru>
2022-04-15 00:17:53 +0300

    Adding Clipping Tool entity

---
+++ ComputerGraphics.Core/Entities/ClippingTool.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGraphics.Core.Entities
+{
+    public sealed class ClippingTool
+    {
+        public string Name { get; set; }
+        public Action Action { get; set; }
+    }
+}
